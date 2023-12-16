59bdb34a6601559272f60f1033b5a8202402b554
nurraihanpratama <mnurraihanpratama@gmail.com>
2023-12-16 19:36:22 +0700

    update logo

---
+++ app/Modules/Admin/Kelas/Tables/KelasDataTable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Modules\Admin\Siswa\Tables;
+
+use App\Models\TblSiswa;
+use App\Modules\Admin\Siswa\Resources\SiswaResource;
+
+class SiswaDataTable
+{
+    public function generate($request)
+    {
+        $data = TblSiswa::query()
+                ->paginate(15);
+
+        $collection = SiswaResource::collection($data);
+
+        return $collection;
+    }
+}
